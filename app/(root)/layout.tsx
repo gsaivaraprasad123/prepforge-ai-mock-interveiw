824c9ba271d09b2f5171139847ffbb0d2e41ad10
Sai Vara Prasad Gandhe <gsaivaraprasad123@gmail.com>
2025-04-23 02:49:41 +0530

    User Auth Implement + code refactors : Firebase

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
+import { isAuthenticated } from '@/lib/actions/auth.action'
 import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React, { ReactNode } from 'react'
 
-const layout = ({children} : {children : ReactNode}) => {
+const layout = async({children} : {children : ReactNode}) => {
+  const isUserAuthenticated = await isAuthenticated();
+
+  if(!isUserAuthenticated) redirect("/sign-in")
+    
   return (
     <div className='root-layout'>
       <nav>
--- firebase/client.ts
+++ firebase/client.ts
@@ -1,19 +1,26 @@
+// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAtJZg8R-X0O-S7C7mfiKcrMcqQo4qWLnk",
-  authDomain: "prepforge-6b18b.firebaseapp.com",
-  projectId: "prepforge-6b18b",
-  storageBucket: "prepforge-6b18b.firebasestorage.app",
-  messagingSenderId: "982212545889",
-  appId: "1:982212545889:web:1ac77297bba78534d50920",
-  measurementId: "G-4RJ92YGE8J"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const analytics = getAnalytics(app);
 
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
